ba5bd0409924c4806c9d5deaaff12c3bcee630ae
Sadid Ahammed <sadidahammedtonoy@gmail.com>
2026-03-01 11:20:12 +0600

    28 Feb 2026

--- lib/Core/snakbar.dart
+++ lib/Core/snakbar.dart
@@ -10,6 +10,7 @@ class AppSnackbar {
     /// Close any previous snackbar (optional)
     if (Get.isSnackbarOpen) Get.closeAllSnackbars();
 
+
     Get.rawSnackbar(
       title: title.isEmpty ? null : title,
       message: message,
